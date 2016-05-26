61a54a901234fd8e98d67b5a0cad082962ff9d0a
markafossen <fossen@gmail.com>
2016-05-26 07:59:51 -0600

    Initial setup, Object STubs. 15 minute committ.

---
+++ KualiCoElevator/App_Code/Elevator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KualiCoElevator
+{
+    public class Elevator
+    {
+        #region Private Properties
+
+
+        #endregion
+
+        #region Public Properties
+        #endregion
+    }
+}
